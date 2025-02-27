c253f6c609750e5e0625281139af41f45ca402fd
ravinder9280 <ravinder92809@gmail.com>
2025-02-27 15:11:03 +0530

    2nd

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com',"images.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 export default nextConfig;
